d758b1f29545010fbd4d609674723241b354b923
Daniil Mironov <daniil.mironov@stfc.ac.uk>
2020-04-08 11:47:48 +0100

    add optional h5_file argument

--- neutron-net/sequencers.py
+++ neutron-net/sequencers.py
@@ -4,9 +4,13 @@ from tensorflow.keras.utils import Sequence
 
 class DataSequence(Sequence):
     ''' Use Keras sequence to load image data from h5 file '''
-    def __init__(self, h5_file, dim, channels, batch_size, mode=None, layers=None):
+    def __init__(self, dim, channels, batch_size, mode=None, layers=None, h5_file=None, labels=None):
         'Initialisation'
+        # Potential Inputs
         self.file       = h5_file
+        self.labels     = labels
+
+        # Parameters
         self.dim        = dim                     # Image dimensions
         self.channels   = channels                # Image channels                   
         self.batch_size = batch_size              # Batch size
@@ -30,30 +34,32 @@ class DataSequence(Sequence):
         images = np.empty((self.batch_size, *self.dim, self.channels))
 
         if self.mode == 'regression':
-            targets_depth = np.empty((self.batch_size, self.layers), dtype=float)
-            targets_sld = np.empty((self.batch_size, self.layers), dtype=float)
+            if self.file:
+                targets_depth = np.empty((self.batch_size, self.layers), dtype=float)
+                targets_sld = np.empty((self.batch_size, self.layers), dtype=float)
 
-            for i, idx in enumerate(indexes):
-                image = self.file['images'][idx]
-                target = self.file['scaled_targets'][idx]
+                for i, idx in enumerate(indexes):
+                    image = self.file['images'][idx]
+                    target = self.file['scaled_targets'][idx]
 
-                images[i,] = image
-                targets_depth[i,] = target[::2]
-                targets_sld[i,] = target[1::2]
-        
+                    images[i,] = image
+                    targets_depth[i,] = target[::2]
+                    targets_sld[i,] = target[1::2]
+            
 
-            return images, {'depth': targets_depth, 'sld': targets_sld}
+                return images, {'depth': targets_depth, 'sld': targets_sld}
 
         if self.mode == 'classification':
-            classes = np.empty((self.batch_size, 1), dtype=int)
+            if self.file:
+                classes = np.empty((self.batch_size, 1), dtype=int)
 
-            for i, idx in enumerate(indexes):
-                image = self.file['images'][idx]
+                for i, idx in enumerate(indexes):
+                    image = self.file['images'][idx]
 
-                images[i,] = image
-                classes[i,] = self.file['classes'][idx]
+                    images[i,] = image
+                    classes[i,] = self.file['classes'][idx]
 
-            return images, classes
+                return images, classes
 
         # for i, idx in enumerate(indexes):
         #     image = self.file['images'][idx]
@@ -73,6 +79,46 @@ class DataSequence(Sequence):
     def on_epoch_end(self):
         'Updates indexes after each epoch'
         self.indexes = np.arange(len(self.file['images']))
+
+    def close_file(self):
+        self.file.close()
+
+class DataSequenceClasses(Sequence):
+    ''' Use Keras sequence to load image data from h5 file '''
+    def __init__(self, labels, dim, channels, batch_size):
+        'Initialisation'
+        self.labels     = labels
+        self.dim        = dim                     # Image dimensions
+        self.channels   = channels                # Image channels                   
+        self.batch_size = batch_size              # Batch size
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes number of batches per epoch'
+        return int(np.floor(len(self.labels.keys()) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generates one batch of data'
+        indexes = self.indexes[index * self.batch_size: (index + 1) * self.batch_size]
+        np_image_batch = [list(self.labels.keys())[k] for k in indexes]
+        x, c = self.__data_generation(np_image_batch)
+
+        return x, c
+
+    def __data_generation(self, np_image_batch):
+        'Generates data containing batch_size samples'
+        x = np.empty((self.batch_size, *self.dim, self.channels))
+        c = np.empty((self.batch_size, 1), dtype=int)
+
+        for i, np_image in enumerate(np_image_batch):
+            x[i,] = np.load(np_image)
+            c[i,] = self.labels[np_image]
+        
+        return x, c        
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.labels.keys()))
 
     def close_file(self):
         self.file.close()
