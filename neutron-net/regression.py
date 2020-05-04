33e93557dabb1ffa10a8d35734245566ed75ebc8
Daniil Mironov <daniil.mironov@stfc.ac.uk>
2020-05-04 11:26:08 +0100

    Add argument parser

--- neutron-net/regression.py
+++ neutron-net/regression.py
@@ -14,7 +14,7 @@ from datetime import datetime
 from sklearn.metrics import mean_squared_error, confusion_matrix
 
 import tensorflow as tf
-from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, LeakyReLU, Dropout, Input
 from tensorflow.keras.utils import Sequence
 from tensorflow.keras.utils import plot_model
@@ -70,3 +70,31 @@ class Sequencer(Sequence):
             np.random.shuffle(indexes)
 
         self.indexes = indexes
+
+def main(args):
+    pass
+
+def parse():
+    parser = argparse.ArgumentParser(description="Keras Regressor Training")
+
+    # Meta Parameters
+    parser.add_argument("data", metavar="PATH", help="path to data directory")
+    parser.add_argument("save", metavar="PATH", help="path to save directory")
+    parser.add_argument("base", metavar="PATH", help="path to base classifier model")
+    parser.add_argument("-l", "--log", action="store_true", help="boolean: log metrics to CometML?")
+    parser.add_argument("-s", "--summary", action="store_true", help="show model summary")
+
+    # Model parameters
+    parser.add_argument("-e", "--epochs", default=2, type=int, metavar="N", help="number of epochs")
+    parser.add_argument("-b", "--batch_size", default=40, type=int, metavar="N", help="no. samples per batch (def:40)")
+    parser.add_argument("-j", "--workers", default=1, type=int, metavar="N", help="no. data loading workers (def:1)")
+
+    # Learning parameters
+    parser.add_argument("-lr", "--learning_rate", default=0.0003, type=float, metavar="R", help="Nadam learning rate")
+    parser.add_argument("-dr", "--dropout_rate", default=0.1, type=float, metavar="R", help="dropout rate" )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse()
+    main(args)
