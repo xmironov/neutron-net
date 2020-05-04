3b7d62c37855f62b89675de30af4bc4ae116be56
Daniil Mironov <daniil.mironov@stfc.ac.uk>
2020-05-04 11:54:34 +0100

    Add data loading

--- neutron-net/regression.py
+++ neutron-net/regression.py
@@ -30,8 +30,9 @@ tf.compat.v1.disable_eager_execution()
 class Sequencer(Sequence):
     """ Use Keras Sequence class to load image data from h5 file"""
     def __init__(self, file, labels, dim, channels, batch_size, layers, debug=False, shuffle=False):
+        self.indexes    = np.where(np.array(file["class"]) == layers)[0]
         self.file       = file
-        self.labels     = labels
+        self.labels     = labels[self.indexes]
         self.dim        = dim
         self.channels   = channels
         self.batch_size = batch_size
@@ -42,7 +43,7 @@ class Sequencer(Sequence):
     
     def __len__(self):
         """ Denotes number of batches per epoch"""
-        return int(np.floor(len(np.where(self.file["class"] == self.layers)[0]) / self.batch_size))
+        return int(np.floor(len(self.indexes) / self.batch_size))
 
     def __getitem__(self, idx):
         """ Generates one batch of data"""
@@ -64,7 +65,7 @@ class Sequencer(Sequence):
     
     def on_epoch_end(self):
         """Updates indexes after each epoch"""
-        indexes = np.where(self.file["class"] == self.layers)[0]
+        indexes = np.where(np.array(self.file["class"]) == self.layers)[0]
 
         if self.shuffle:
             np.random.shuffle(indexes)
@@ -72,7 +73,30 @@ class Sequencer(Sequence):
         self.indexes = indexes
 
 def main(args):
-    pass
+    name = "regressor-%s[" % str(args.layers) + datetime.now().strftime("%Y-%m-%dT%H%M%S") + "]"
+    save = r"C:\Users\mtk57988\stfc\neutron-net\neutron-net\models\investigate"
+    base = r"C:\Users\mtk57988\stfc\neutron-net\neutron-net\models\investigate\classifier-[2020-05-03T104626]\full_model.h5"
+    data = r"C:\Users\mtk57988\stfc\ml-neutron\neutron_net\data\perfect_w_classes\all"
+
+    savepath = os.path.join(save, name)
+
+    if args.log:
+        experiment = Experiment(api_key="Qeixq3cxlTfTRSfJ2hyPlMWjk", project_name="general", workspace="xandrovich")
+    
+    train_dir = os.path.join(data, "train.h5")
+    validate_dir = os.path.join(data, "valid.h5")
+    test_dir = os.path.join(data, "test.h5")
+
+    train_file = h5py.File(train_dir, "r")
+    validate_file = h5py.File(validate_dir, "r")
+    test_file = h5py.File(test_dir, "r")
+
+    targets = load_targets(data)
+    train_targets, validate_targets, test_targets = targets["train"], targets["valid"], targets["test"]
+
+    train_loader = Sequencer(train_file, train_targets, DIMS, CHANNELS, args.batch_size, args.layers)
+    validate_loader = Sequencer(validate_file, validate_targets, DIMS, CHANNELS, args.batch_size, args.layers)
+    test_loader = Sequencer(test_file, test_targets, DIMS, CHANNELS, args.batch_size, args.layers)
 
 def parse():
     parser = argparse.ArgumentParser(description="Keras Regressor Training")
@@ -81,6 +105,7 @@ def parse():
     parser.add_argument("data", metavar="PATH", help="path to data directory")
     parser.add_argument("save", metavar="PATH", help="path to save directory")
     parser.add_argument("base", metavar="PATH", help="path to base classifier model")
+    parser.add_argument("layers", metavar="N", type=int, help="no. layers of system")
     parser.add_argument("-l", "--log", action="store_true", help="boolean: log metrics to CometML?")
     parser.add_argument("-s", "--summary", action="store_true", help="show model summary")
 
@@ -94,6 +119,13 @@ def parse():
     parser.add_argument("-dr", "--dropout_rate", default=0.1, type=float, metavar="R", help="dropout rate" )
     return parser.parse_args()
 
+def load_targets(path):
+    data = {}
+    for section in ["train", "valid", "test"]:
+        with h5py.File(os.path.join(path, "{}.h5".format(section)), "r") as f:
+            data["{}".format(section)] = np.array(f["scaledY"])
+
+    return data
 
 if __name__ == "__main__":
     args = parse()
