Fixed problem with scaling for x-rays

neutron-net/generate_data.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage import color
 
+from generate_refnx import CurveGenerator, NeutronGenerator, XRayGenerator
+
 LAYERS_STR = {1: "one", 2: "two", 3: "three"}
 DIMS       = (300, 300)
 CHANNELS   = 1
@@ -18,26 +20,34 @@ class ImageGenerator:
     
     Class Attributes:
         depth_bounds (tuple): bounds on depths used for scaling targets.
-        sld_bounds (tuple): bounds on SLDs used for scaling targets.
-        x-ray (Boolean): whether the input reflectivity data is neutron or x-ray.
+        sld_neutron_bounds (tuple): bounds on SLDs used for scaling neutron data targets.
+        x-sld_xray_bounds (tuple): bounds on SLDs used for scaling x-ray data targets.
         
     """
-    depth_bounds = (20, 3000)
-    sld_bounds   = (-1, 10)
+    depth_bounds       = CurveGenerator.thick_bounds
+    sld_neutron_bounds = NeutronGenerator.sld_bounds
+    sld_xray_bounds    = (XRayGenerator.density_constant * XRayGenerator.density_bounds[0], 
+                          XRayGenerator.density_constant * XRayGenerator.density_bounds[1])
 
     @staticmethod
-    def scale_targets(concatenated):
+    def scale_targets(concatenated, xray=False):
         """Scales target (SLD and depth) values to be in the range [0,1].
 
         Args:
             concatenated (Dataset): h5py dataset containing train, validate and test data.
+            xray (Boolean): whether the data is x-ray or neutron.
 
         """
         for split, data in concatenated.items(): #Iterate over each split.
             scaled_targets = np.zeros(data['targets'].shape) #Blank array of zeros to fill in.
             for i in range(3): #Apply scaling to the depth and SLD values for each layer.
-                scaled_targets[:, 2*i]   = ImageGenerator.scale_to_range(data['targets'][:, 2*i],   ImageGenerator.depth_bounds, (0, 1))
-                scaled_targets[:, 2*i+1] = ImageGenerator.scale_to_range(data['targets'][:, 2*i+1], ImageGenerator.sld_bounds,   (0, 1))
+                scaled_targets[:, 2*i] = ImageGenerator.scale_to_range(data['targets'][:, 2*i], ImageGenerator.depth_bounds, (0, 1))
+                
+                if xray:
+                    scaled_targets[:, 2*i+1] = ImageGenerator.scale_to_range(data['targets'][:, 2*i+1], ImageGenerator.sld_xray_bounds, (0, 1))
+                else:
+                    scaled_targets[:, 2*i+1] = ImageGenerator.scale_to_range(data['targets'][:, 2*i+1], ImageGenerator.sld_neutron_bounds, (0, 1))
+                    
             concatenated[split]['targets_scaled'] = scaled_targets
 
     @staticmethod
@@ -252,7 +262,7 @@ def generate_images(data_path, save_path, layers, xray=False, chunk_size=1000, d
     del split_data
 
     ImageGenerator.shuffle_data(concatenated)  #Shuffle concatenated data
-    ImageGenerator.scale_targets(concatenated) #Scale the targets to be between 0 and 1.
+    ImageGenerator.scale_targets(concatenated, xray) #Scale the targets to be between 0 and 1.
 
     shapes = ImageGenerator.get_shapes(concatenated, chunk_size=chunk_size) #Get dataset shapes for use in defining chunk sizes.
     

neutron-net/pipeline.py
@@ -12,7 +12,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.utils  import Sequence
 
 from refnx.dataset  import ReflectDataset
-from refnx.reflect  import SLD, ReflectModel
+from refnx.reflect  import SLD, MaterialSLD, ReflectModel
 from refnx.analysis import Objective, CurveFitter
 
 from generate_refnx import NeutronGenerator, XRayGenerator
@@ -173,12 +173,13 @@ class KerasDropoutPredicter():
             self.f_3 = K.function([models[3].layers[0].input, K.learning_phase()],
                                   [models[3].layers[-2].output, models[3].layers[-1].output])
 
-    def predict(self, sequence, n_iter=5):
+    def predict(self, sequence, n_iter=5, xray=False):
         """Makes Bayesian-like predictions using given models.
 
         Args:
             sequence (DataLoaderRegression): the sequence providing data to predict on.
             n_iter (int): the number of iterations per step.
+            xray (Boolean): whether data to predict on uses a neutron or x-ray probe.
 
         Returns:
             List of depth and SLD predictions along with errors associated with each.
@@ -203,7 +204,11 @@ class KerasDropoutPredicter():
                     [depths, slds] = self.f_3([x, 1])
                     
                 depth_scaled = ImageGenerator.scale_to_range(depths, (0, 1), ImageGenerator.depth_bounds)
-                sld_scaled   = ImageGenerator.scale_to_range(slds,   (0, 1), ImageGenerator.sld_bounds)
+                if xray:
+                    sld_scaled = ImageGenerator.scale_to_range(slds, (0, 1), ImageGenerator.sld_xray_bounds)
+                else:
+                    sld_scaled = ImageGenerator.scale_to_range(slds, (0, 1), ImageGenerator.sld_neutron_bounds)
+                    
                 results.append([depth_scaled, sld_scaled])
 
             results = np.array(results)
@@ -248,7 +253,7 @@ class Model():
     dq        = 2
     scale     = 1
 
-    def __init__(self, file_path, layers, predicted_slds, predicted_depths):
+    def __init__(self, file_path, layers, predicted_slds, predicted_depths, xray):
         """Initialises the Model class by creating a refnx model with given predicted values.
 
         Args:
@@ -256,19 +261,32 @@ class Model():
             layers (int): the number of layers for the model predicted by the classifier.
             predicted_slds (ndarray): an array of predicted SLDs for each layer.
             predicted_depths (ndarray): an array of predicted depths for each layer.
+            xray (Boolean): whether the model should use a neutron or x-ray probe.
 
         """
         self.structure = SLD(0, name='Air') #Model starts with air.
 
-        for i in range(layers):
-            layer = SLD(predicted_slds[i], name='Layer {}'.format(i+1))(thick=predicted_depths[i], rough=Model.roughness)
-            layer.sld.real.setp(bounds=ImageGenerator.sld_bounds, vary=True)
-            layer.thick.setp(bounds=ImageGenerator.depth_bounds,  vary=True)
-            self.structure = self.structure | layer  #Next comes each layer.
-
-        si_substrate = SLD(Model.si_sld, name='Si Substrate')(thick=0, rough=Model.roughness) #Then substrate
+        if xray:
+            for i in range(layers):
+                density = predicted_slds[i] / XRayGenerator.density_constant
+                SLD_layer = MaterialSLD(XRayGenerator.material, density, probe='x-ray', wavelength=XRayGenerator.wavelength, name='Layer {}'.format(i+1))
+                layer = SLD_layer(thick=predicted_depths[i], rough=Model.roughness)
+                layer.density.setp(bounds=XRayGenerator.density_bounds, vary=True)
+                layer.thick.setp(bounds=ImageGenerator.depth_bounds, vary=True)
+                self.structure = self.structure | layer  #Next comes each layer.
+            #Then substrate    
+            si_substrate = MaterialSLD(XRayGenerator.material, XRayGenerator.substrate_density, probe='x-ray', name='Si Substrate')(thick=0, rough=Model.roughness)
+            
+        else:
+            for i in range(layers):
+                layer = SLD(predicted_slds[i], name='Layer {}'.format(i+1))(thick=predicted_depths[i], rough=Model.roughness)
+                layer.sld.real.setp(bounds=ImageGenerator.sld_bounds, vary=True)
+                layer.thick.setp(bounds=ImageGenerator.depth_bounds,  vary=True)
+                self.structure = self.structure | layer  #Next comes each layer.
+            #Then substrate
+            si_substrate = SLD(Model.si_sld, name='Si Substrate')(thick=0, rough=Model.roughness)
+            
         self.structure = self.structure | si_substrate
-
         data = ReflectDataset(file_path) #Load the data for which the model is designed for.
         self.model = ReflectModel(self.structure, scale=Model.scale, dq=Model.dq)
         self.objective = Objective(self.model, data)
@@ -337,7 +355,7 @@ class Pipeline:
     """The Pipeline class can perform data generation, training and predictions."""
 
     @staticmethod
-    def run(data_path, save_path, classifier_path, regressor_paths, fit=True, n_iter=5):
+    def run(data_path, save_path, classifier_path, regressor_paths, fit=True, n_iter=5, xray=False):
         """Performs classification and regression to create a refnx model for given .dat files.
 
         Args:
@@ -347,6 +365,7 @@ class Pipeline:
             regressor_paths (dict): dictionary of paths to regressors for each layer.
             fit (Boolean): whether to fit the newly generated models.
             n_iter (int): number of times to predict using the KDP.
+            xray (Boolean): whether the .dat files are x-ray or neutron.
 
         Returns:
             A dictionary of models, index by filename, initialized with predicted values for each .dat file.
@@ -362,7 +381,7 @@ class Pipeline:
             print(">>> Predicted number of layers: {}\n".format(layer_predictions[curve]))
 
         #Use regression to predict the SLDs and depths for each file.
-        sld_predictions, depth_predictions, sld_errors, depth_errors = Pipeline.__regress(data_path, save_path, regressor_paths, layer_predictions, npy_image_filenames, n_iter)
+        sld_predictions, depth_predictions, sld_errors, depth_errors = Pipeline.__regress(data_path, save_path, regressor_paths, layer_predictions, npy_image_filenames, n_iter, xray)
         
         models = {} 
         #Print the predictions and errors for the depths and SLDs for each layer for each file.
@@ -374,7 +393,7 @@ class Pipeline:
                 print(">>> Predicted layer {0} - Depth: {1:10.4f} | Error: {2:10.6f}".format(i+1, depth_predictions[curve][i], depth_errors[curve][i]))
 
             #Create a refnx model with the predicted number of layers, SLDs and depths.
-            model = Model(dat_files[curve], layer_predictions[curve], sld_predictions[curve], depth_predictions[curve])
+            model = Model(dat_files[curve], layer_predictions[curve], sld_predictions[curve], depth_predictions[curve], xray)
             model.plot_objective(prediction=True)
             models[filename] = model
             print()
@@ -407,7 +426,7 @@ class Pipeline:
         return np.argmax(classifier.predict(classifier_loader, verbose=1), axis=1), npy_image_filenames #Make predictions
 
     @staticmethod
-    def __regress(data_path, save_path, regressor_paths, layer_predictions, npy_image_filenames, n_iter):
+    def __regress(data_path, save_path, regressor_paths, layer_predictions, npy_image_filenames, n_iter, xray=False):
         """Performs SLD and depth regression for specified .dat files.
 
         Args:
@@ -418,6 +437,7 @@ class Pipeline:
             npy_image_filenames (ndarray): an array of filenames of files containing images
                                            corresponding to the input .dat files.
             n_iter (int): number of times to predict using the KDP.
+            xray (Boolean): whether the .dat files are x-ray or neutron.
                               
         Returns:
             SLD and depth predictions along with the errors for each.
@@ -436,7 +456,7 @@ class Pipeline:
 
         #Use custom class to activate Dropout at test time in models
         kdp = KerasDropoutPredicter(regressors)
-        kdp_predictions = kdp.predict(loader, n_iter=n_iter)
+        kdp_predictions = kdp.predict(loader, n_iter, xray)
 
         depth_predictions = kdp_predictions[0][0]
         sld_predictions   = kdp_predictions[0][1]
@@ -571,11 +591,11 @@ class Pipeline:
             data_path_layer = save_path + "/data/{}".format(LAYERS_STR[layer])
             if train_regressor:
                 print(">>> Training {}-layer regressor".format(LAYERS_STR[layer]))
-                regress(data_path_layer, layer, save_path, epochs=regressor_epochs, show_plots=show_plots) #Train the regressor.
+                regress(data_path_layer, layer, save_path, epochs=regressor_epochs, show_plots=show_plots, xray=xray) #Train the regressor.
             else:
                 print(">>> Loading {}-layer regressor".format(LAYERS_STR[layer]))
                 load_path_layer = save_path + "/{}-layer-regressor/full_model.h5".format(LAYERS_STR[layer]) #Load an existing regressor.
-                regress(data_path_layer, layer, load_path=load_path_layer, train=False, show_plots=show_plots)
+                regress(data_path_layer, layer, load_path=load_path_layer, train=False, show_plots=show_plots, xray=xray)
             print()
 
 
@@ -598,4 +618,4 @@ if __name__ == "__main__":
     classifier_path = load_path + "/classifier/full_model.h5"
     layers = 3
     regressor_paths = {i: load_path + "/{}-layer-regressor/full_model.h5".format(LAYERS_STR[i]) for i in range(1, layers+1)}
-    models = Pipeline.run(data_path, save_path, classifier_path, regressor_paths, fit=True, n_iter=100)
+    models = Pipeline.run(data_path, save_path, classifier_path, regressor_paths, fit=True, n_iter=100, xray=xray)

neutron-net/plotting.py
@@ -24,13 +24,14 @@ class KerasDropoutPredicter():
             [model.layers[0].input, K.learning_phase()],
             [model.layers[-2].output, model.layers[-1].output])
 
-    def predict(self, sequencer, steps=None, n_iter=5):
+    def predict(self, sequencer, steps=None, n_iter=5, xray=False):
         """Makes Bayesian-like predictions using give model.
 
         Args:
             sequencer (DataLoader): the sequence providing data to predict on.
             steps (int): the number of batches of data to plot.
             n_iter (int): the number of iterations per step.
+            xray (Boolean): whether input data is neutron or x-ray.
 
         Returns:
             List of numpy arrays of depth and SLD predictions, labels and associated errors.
@@ -51,14 +52,24 @@ class KerasDropoutPredicter():
                 [depths, slds] = self.f([images, 1]) #Set Dropout to True: 1
                 #De-scale predictions
                 depth_unscaled = ImageGenerator.scale_to_range(depths, (0, 1), ImageGenerator.depth_bounds)
-                sld_unscaled   = ImageGenerator.scale_to_range(slds,   (0, 1), ImageGenerator.sld_bounds)
+                
+                if xray:
+                    sld_unscaled = ImageGenerator.scale_to_range(slds, (0, 1), ImageGenerator.sld_xray_bounds)
+                else:
+                    sld_unscaled = ImageGenerator.scale_to_range(slds, (0, 1), ImageGenerator.sld_neutron_bounds)
+                    
                 results.append([depth_unscaled, sld_unscaled])
 
             results = np.array(results)
             prediction, uncertainty = results.mean(axis=0), results.std(axis=0)
             #De-scale targets
             targets_depth = ImageGenerator.scale_to_range(targets["depth"], (0, 1), ImageGenerator.depth_bounds)
-            targets_sld   = ImageGenerator.scale_to_range(targets["sld"],   (0, 1), ImageGenerator.sld_bounds)
+            
+            if xray:
+                targets_sld = ImageGenerator.scale_to_range(targets["sld"], (0, 1), ImageGenerator.sld_xray_bounds)
+            else:
+                targets_sld = ImageGenerator.scale_to_range(targets["sld"], (0, 1), ImageGenerator.sld_neutron_bounds)
+                
             targets_sum = np.array([targets_depth, targets_sld])
             outs = np.array([prediction, uncertainty, targets_sum])
 
@@ -91,17 +102,23 @@ class Plotter:
 
     Class Attributes:
         depth_axis (tuple): the range of depth values for the plot.
-        sld_axis (tuple): the range of SLD values for the plot.
+        sld_neutron_axis (tuple): the range of neutron SLD values for the plot.
+        sld_xray_axis (tuple): the range of xray SLD values for the plot.
         depth_ticks (tuple): the values to place ticks on the depth axis.
-        sld_ticks (tuple): the values to place ticks on the SLD axis.
+        sld_neutron_ticks (tuple): the values to place ticks on the neutron SLD axis.
+        sld_xray_ticks (tuple): the values to place ticks on the x-ray SLD axis.
         pad (int): padding value for the layer annotations.
         v_pad (int): vertical padding value for the layer annotations.
 
     """
-    depth_axis  = (-250, 3250)
-    sld_axis    = (-1.5, 11)
-    depth_ticks = (0, 1000, 2000, 3000)
-    sld_ticks   = (0, 2.5, 5, 7.5, 10)
+    depth_axis       = (-250, 3250)
+    sld_neutron_axis = (-1.5, 11)
+    sld_xray_axis    = (5e-6, 0.00016)
+    
+    depth_ticks       = (0, 1000, 2000, 3000)
+    sld_neutron_ticks = (0, 2.5, 5, 7.5, 10)
+    sld_xray_ticks    = (2e-5, 6e-5, 10e-5, 1.4e-4, 1.8e-4)
+    
     pad   = 55
     v_pad = 80
 
@@ -119,18 +136,12 @@ class Plotter:
         """
         ax.errorbar(labels, preds, errors, fmt="o", mec="k", mew=0.5, alpha=0.6, capsize=3, color="b", zorder=-130, markersize=4)
         ax.plot([0,1], [0,1], 'k', transform=ax.transAxes)
-        ax.set_xlim(Plotter.depth_axis)
-        ax.set_ylim(Plotter.depth_axis)
-        ax.set_yticks(Plotter.depth_ticks)
-        ax.set_yticklabels(Plotter.depth_ticks)
-        ax.set_xticks(Plotter.depth_ticks)
-        ax.set_xticklabels(Plotter.depth_ticks)
         ax.set_ylabel("$\mathregular{Depth_{predict}\ (Å)}$", fontsize=11, weight="bold")
         if x_axis_label:
             ax.set_xlabel("$\mathregular{Depth_{true}\ (Å)}$", fontsize=10, weight="bold")
 
     @staticmethod
-    def __sld_subplot(ax, labels, preds, errors, x_axis_label=False):
+    def __sld_subplot(ax, labels, preds, errors, x_axis_label=False, xray=False):
         """Plots a ground truth against prediction plot for SLDs of a given layer.
 
         Args:
@@ -139,28 +150,39 @@ class Plotter:
             preds (ndarray): predictions for SLDs.
             errors (ndarray): errors in predictions for SLDs.
             x_axis_label (Boolean): whether to add the x-axis label or not.
+            xray (Boolean): whether data to plot is x-ray or neutron.
 
         """
         ax.errorbar(labels, preds, errors, fmt="o", mec="k", mew=0.5, alpha=0.6, capsize=3, color="g", zorder=-130, markersize=4)
         ax.plot([0,1], [0,1], 'k', transform=ax.transAxes)
-        ax.set_xlim(Plotter.sld_axis)
-        ax.set_ylim(Plotter.sld_axis)
-        ax.set_yticks(Plotter.sld_ticks)
-        ax.set_yticklabels(Plotter.sld_ticks)
-        ax.set_xticks(Plotter.sld_ticks)
-        ax.set_xticklabels(Plotter.sld_ticks)
+        if xray:
+            ax.set_xlim(Plotter.sld_xray_axis)
+            ax.set_ylim(Plotter.sld_xray_axis)
+            ax.set_yticks(Plotter.sld_xray_ticks)
+            ax.set_yticklabels(Plotter.sld_xray_ticks)
+            ax.set_xticks(Plotter.sld_xray_ticks)
+            ax.set_xticklabels(Plotter.sld_xray_ticks)
+        else:
+            ax.set_xlim(Plotter.sld_neutron_axis)
+            ax.set_ylim(Plotter.sld_neutron_axis)
+            ax.set_yticks(Plotter.sld_neutron_ticks)
+            ax.set_yticklabels(Plotter.sld_neutron_ticks)
+            ax.set_xticks(Plotter.sld_neutron_ticks)
+            ax.set_xticklabels(Plotter.sld_neutron_ticks)
+            
         ax.set_ylabel("$\mathregular{SLD_{predict}\ (Å^{-2})}$", fontsize=11, weight="bold")
         if x_axis_label:
             ax.set_xlabel("$\mathregular{SLD_{true}\ (Å^{-2})}$", fontsize=10, weight="bold")
 
     @staticmethod
-    def __one_layer_plot(preds, labels, errors):
+    def __one_layer_plot(preds, labels, errors, xray=False):
         """Plots ground truths against predictions for depths and SLDs of 1-layer curves.
 
         Args:
             preds (ndarray): KDP predictions for depth and SLD.
             labels (ndarray): corresponding ground truths for depth and SLD predictions.
             errors (ndarray): errors in depth and SLD KDP predictions.
+            xray (Boolean): whether data uses a x-ray or neutron probe.
 
         """
         fig, (ax_depth, ax_sld) = plt.subplots(1, 2, figsize=(8,4.5)) #Make the subplot structure for one layer.
@@ -169,7 +191,7 @@ class Plotter:
 
         #Add depth and SLD subplots for the single layer
         Plotter.__depth_subplot(ax_depth, labels[:,0], preds[:,0], errors[:,0], x_axis_label=True)
-        Plotter.__sld_subplot(ax_sld,     labels[:,1], preds[:,1], errors[:,1], x_axis_label=True)
+        Plotter.__sld_subplot(ax_sld,     labels[:,1], preds[:,1], errors[:,1], x_axis_label=True, xray=xray)
         ax_depth.annotate("Layer 1", xy=(0, 0.5),
                           xytext=(-ax_depth.yaxis.labelpad - Plotter.pad, Plotter.v_pad),
                           xycoords="axes points", textcoords="offset points",
@@ -178,13 +200,14 @@ class Plotter:
         plt.show()
 
     @staticmethod
-    def __two_layer_plot(preds, labels, errors):
+    def __two_layer_plot(preds, labels, errors, xray=False):
         """Plots ground truths against predictions for depths and SLDs of 2-layer curves.
 
         Args:
             preds (ndarray): KDP predictions for depth and SLD.
             labels (ndarray): corresponding ground truths for depth and SLD predictions.
             errors (ndarray): errors in depth and SLD KDP predictions.
+            xray (Boolean): whether data uses a x-ray or neutron probe.
 
         """
         fig, axes = plt.subplots(2, 2, figsize=(8,8)) #Make the subplot structure for two layers.
@@ -193,7 +216,7 @@ class Plotter:
 
         #Add depth and SLD subplots for the first layer
         Plotter.__depth_subplot(axes[0][0], labels[:,0], preds[:,0], errors[:,0])
-        Plotter.__sld_subplot(axes[0][1],   labels[:,1], preds[:,1], errors[:,1])
+        Plotter.__sld_subplot(axes[0][1],   labels[:,1], preds[:,1], errors[:,1], xray=xray)
         axes[0][0].annotate("Layer 1",
                             xy=(0, 0.5), xytext=(-axes[0][0].yaxis.labelpad - Plotter.pad, Plotter.v_pad),
                             xycoords="axes points", textcoords="offset points",
@@ -201,7 +224,7 @@ class Plotter:
 
         #Add depth and SLD subplots for the second layer
         Plotter.__depth_subplot(axes[1][0], labels[:,2], preds[:,2], errors[:,2], x_axis_label=True)
-        Plotter.__sld_subplot(axes[1][1],   labels[:,3], preds[:,3], errors[:,3], x_axis_label=True)
+        Plotter.__sld_subplot(axes[1][1],   labels[:,3], preds[:,3], errors[:,3], x_axis_label=True, xray=xray)
         axes[1][0].annotate("Layer 2", xy=(0, 0.5),
                             xytext=(-axes[1][0].yaxis.labelpad - Plotter.pad, Plotter.v_pad),
                             xycoords="axes points", textcoords="offset points",
@@ -210,13 +233,14 @@ class Plotter:
         plt.show()
 
     @staticmethod
-    def __three_layer_plot(preds, labels, errors):
+    def __three_layer_plot(preds, labels, errors, xray=False):
         """Plots ground truths against predictions for depths and SLDs of 3-layer curves.
 
         Args:
             preds (ndarray): KDP predictions for depth and SLD.
             labels (ndarray): corresponding ground truths for depth and SLD predictions.
             errors (ndarray): errors in depth and SLD KDP predictions.
+            xray (Boolean): whether data uses a x-ray or neutron probe.
 
         """
         fig, axes = plt.subplots(3, 2, figsize=(8,9)) #Make the subplot structure for three layers.
@@ -225,7 +249,7 @@ class Plotter:
 
         #Add depth and SLD subplots for the first layer
         Plotter.__depth_subplot(axes[0][0], labels[:,0], preds[:,0], errors[:,0])
-        Plotter.__sld_subplot(axes[0][1],   labels[:,1], preds[:,1], errors[:,1])
+        Plotter.__sld_subplot(axes[0][1],   labels[:,1], preds[:,1], errors[:,1], xray=xray)
         axes[0][0].annotate("Layer 1", xy=(0, 0.5),
                             xytext=(-axes[0][0].yaxis.labelpad - Plotter.pad, Plotter.v_pad),
                             xycoords="axes points", textcoords="offset points",
@@ -233,7 +257,7 @@ class Plotter:
 
         #Add depth and SLD subplots for the second layer
         Plotter.__depth_subplot(axes[1][0], labels[:,2], preds[:,2], errors[:,2])
-        Plotter.__sld_subplot(axes[1][1],   labels[:,3], preds[:,3], errors[:,3])
+        Plotter.__sld_subplot(axes[1][1],   labels[:,3], preds[:,3], errors[:,3], xray=xray)
         axes[1][0].annotate("Layer 2", xy=(0, 0.5),
                             xytext=(-axes[1][0].yaxis.labelpad - Plotter.pad, Plotter.v_pad),
                             xycoords="axes points", textcoords="offset points",
@@ -241,7 +265,7 @@ class Plotter:
 
         #Add depth and SLD subplots for the third layer
         Plotter.__depth_subplot(axes[2][0], labels[:,4], preds[:,4], errors[:,4], x_axis_label=True)
-        Plotter.__sld_subplot(axes[2][1],   labels[:,5], preds[:,5], errors[:,5], x_axis_label=True)
+        Plotter.__sld_subplot(axes[2][1],   labels[:,5], preds[:,5], errors[:,5], x_axis_label=True, xray=xray)
         axes[2][0].annotate("Layer 3", xy=(0, 0.5),
                             xytext=(-axes[1][0].yaxis.labelpad - Plotter.pad, Plotter.v_pad),
                             xycoords="axes points", textcoords="offset points",
@@ -250,7 +274,7 @@ class Plotter:
         plt.show()      
 
     @staticmethod
-    def kdp_plot(data_path, load_paths, steps=None, batch_size=20, n_iter=100):
+    def kdp_plot(data_path, load_paths, steps=None, batch_size=20, n_iter=100, xray=False):
         """Performs dropout at test time to make Bayesian-like predictions and plots the results.
 
         Args:
@@ -259,6 +283,7 @@ class Plotter:
             steps (int): the number of batches of data to plot.
             batch_size (int): the batch_size for loading data to test with.
             n_iter (int): the number of times to perform a prediction on a single curve (mean is taken of these).
+            xray (Boolean): whether input data is neutron or x-ray.
 
         """
         layers = len(load_paths.values()) #Get the number of layers (either 2 or 3).
@@ -269,7 +294,7 @@ class Plotter:
             loader = DataLoader(file, DIMS, CHANNELS, batch_size, layer)
             kdp    = KerasDropoutPredicter(model, loader)
 
-            preds = kdp.predict(loader, steps=steps, n_iter=n_iter) #Perform KDP predictions and format results.
+            preds = kdp.predict(loader, steps, n_iter, xray) #Perform KDP predictions and format results.
             depth, sld,              = preds[0][0], preds[0][1]
             depth_ground, sld_ground = preds[2][0], preds[2][1]
             depth_std, sld_std       = preds[1][0], preds[1][1]
@@ -290,15 +315,17 @@ class Plotter:
 
             #Call the relevant method for each layer.
             if layer == 1:
-                Plotter.__one_layer_plot(preds_padded,   labels_padded, errors_padded)
+                Plotter.__one_layer_plot(preds_padded, labels_padded, errors_padded, xray)
             elif layer == 2:
-                Plotter.__two_layer_plot(preds_padded,   labels_padded, errors_padded)
+                Plotter.__two_layer_plot(preds_padded, labels_padded, errors_padded, xray)
             elif layer == 3:
-                Plotter.__three_layer_plot(preds_padded, labels_padded, errors_padded)
+                Plotter.__three_layer_plot(preds_padded, labels_padded, errors_padded, xray)
 
 
 if __name__ == "__main__":
     layers = 3
+    xray   = False
     data_path  = "./models/investigate/data"
     load_paths = {i: "./models/investigate/{}-layer-regressor/full_model.h5".format(LAYERS_STR[i]) for i in range(1, layers+1)}
-    Plotter.kdp_plot(data_path, load_paths, steps=10, n_iter=100)
+    
+    Plotter.kdp_plot(data_path, load_paths, steps=10, n_iter=100, xray=xray)

neutron-net/regression.py
@@ -210,18 +210,27 @@ class Regressor():
             os.makedirs(save_path)
         self.model.save(os.path.join(save_path, 'full_model.h5'))
 
-    def plot(self, labels, test_seq):
+    def plot(self, labels, test_seq, xray=False):
         """Plots ground truth depths and SLDs against predictions for each layer.
 
         Args:
             labels (ndarray): an array of ground truth labels.
             test_seq (DataLoader): the test set to predict on.
+            xray (Boolean): whether data is neutron or x-ray.
 
         """
         #Make predictions on test set and descale.
         scaled_preds = self.model.predict(test_seq, use_multiprocessing=False, verbose=1)
         depths = ImageGenerator.scale_to_range(scaled_preds[0], (0, 1), ImageGenerator.depth_bounds)
-        slds   = ImageGenerator.scale_to_range(scaled_preds[1], (0, 1), ImageGenerator.sld_bounds)
+        
+        depth_plot_range = (-100, 3100)
+        
+        if xray:
+            slds = ImageGenerator.scale_to_range(scaled_preds[1], (0, 1), ImageGenerator.sld_xray_bounds)
+            sld_plot_range = (5e-6, 0.00016)
+        else:
+            slds = ImageGenerator.scale_to_range(scaled_preds[1], (0, 1), ImageGenerator.sld_neutron_bounds)
+            sld_plot_range = (-1.5, 10.5)
 
         preds = np.zeros((len(depths[:,0]), 2*self.outputs)) #Format predictions into a single array
         for i in range(self.outputs):
@@ -259,8 +268,8 @@ class Regressor():
                 if k // 2 == self.outputs-1: #Only add ground truth label to bottom subplot
                     ax.set_xlabel("$\mathregular{Depth_{true}\ (Å)}$", fontsize=10, weight="bold")
                 ax.set_ylabel("$\mathregular{Depth_{predict}\ (Å)}$", fontsize=11, weight="bold")
-                ax.set_xlim(-100, 3100)
-                ax.set_ylim(-100, 3100)
+                ax.set_xlim(*depth_plot_range)
+                ax.set_ylim(*depth_plot_range)
                 ax.annotate(row_headers[k//2], xy=(0, 0.5), xytext=(-ax.yaxis.labelpad - pad, 0),
                             xycoords=ax.yaxis.label, textcoords="offset points",
                             size="large", ha="right", va="center")
@@ -268,8 +277,8 @@ class Regressor():
                 if k // 2 == self.outputs-1: #Only add ground truth label to bottom subplot
                     ax.set_xlabel("$\mathregular{SLD_{true}\ (Å^{-2})}$", fontsize=10, weight="bold")
                 ax.set_ylabel("$\mathregular{SLD_{predict}\ (Å^{-2})}$", fontsize=11, weight="bold")
-                ax.set_xlim(-1.5, 10.5)
-                ax.set_ylim(-1.5, 10.5)
+                ax.set_xlim(*sld_plot_range)
+                ax.set_ylim(*sld_plot_range)
             
         plt.show()
 
@@ -279,7 +288,7 @@ class Regressor():
 
 
 def regress(data_path, layer, save_path=None, load_path=None, train=True, summary=False, epochs=2,
-         learning_rate=0.0004, batch_size=20, dropout_rate=0.1, workers=1, show_plots=True):
+         learning_rate=0.0004, batch_size=20, dropout_rate=0.1, workers=1, show_plots=True, xray=False):
     """Either creates a regressor or loads an existing regressor, optionally
        trains the network and then evaluates it.
 
@@ -296,6 +305,7 @@ def regress(data_path, layer, save_path=None, load_path=None, train=True, summar
         dropout_rate (float): the value of the dropout rate hyperparameter.
         workers (int): the number of workers to use.
         show_plots (Boolean): whether to display regression plots.
+        xray (Boolean): whether input data uses a neutron or x-ray probe.
 
     """
     if save_path is not None: #If a save path is provided, save the regressor under a directory
@@ -326,7 +336,7 @@ def regress(data_path, layer, save_path=None, load_path=None, train=True, summar
 
     if show_plots:
         test_labels = np.array(test_h5['targets'])
-        model.plot(test_labels, test_loader)
+        model.plot(test_labels, test_loader, xray)
 
     train_h5.close()
     val_h5.close()
@@ -335,10 +345,11 @@ def regress(data_path, layer, save_path=None, load_path=None, train=True, summar
 
 if __name__ == "__main__":
     layer     = 1
+    xray      = False
     data_path = "./models/investigate/data/{}".format(LAYERS_STR[layer])
     save_path = "./models/investigate"
     load_path = "./models/investigate/{}-layer-regressor/full_model.h5".format(LAYERS_STR[layer])
     
-    regress(data_path, layer, save_path, train=True, epochs=15) #Train new
-    #regress(data_path, layer, save_path, load_path=load_path, train=True, epochs=15) #Train existing
-    #regress(data_path, layer, load_path=load_path, train=False) #Load but do not train existing
+    regress(data_path, layer, save_path, train=True, epochs=15, xray=xray) #Train new
+    #regress(data_path, layer, save_path, load_path=load_path, train=True, epochs=15, xray=xray) #Train existing
+    #regress(data_path, layer, load_path=load_path, train=False, xray=xray) #Load but do not train existing